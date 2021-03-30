eaf9c1653346257ae40aedbe9f9cfd9f426d7df4
Dmitry Evseev <ew.dim@mail.ru>
2021-03-31 03:01:57 +0700

    added function generate raw data

---
+++ app/generate_data_postgres.py
@@ -0,0 +1,19 @@
+from Errors import error_db_handler
+from select_data.utils import (
+    timer,
+    init_postgres
+)
+
+
+@error_db_handler
+@init_postgres
+@timer
+def generate_data(postgres_db, list_count_rows):
+    postgres_db.select_data("""
+        SELECT
+            {}
+    """.format(
+        ",".join(f'filling_of_raw_data_tables({count})' for count in list_count_rows)
+    ))
+
+    return True
