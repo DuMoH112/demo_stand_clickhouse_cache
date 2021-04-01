45b7b8762fe644e6529d2720cec778752daa4adb
Dmitry Evseev <ew.dim@mail.ru>
2021-04-02 04:20:06 +0700

    create functions log_time and random_date

--- app/select_data/utils.py
+++ app/select_data/utils.py
@@ -1,42 +1,57 @@
 import os
-from time import time
+import time
 
 from Database.postgres import Postgres_db
 from Database.clickhouse import Clickhouse_db
 
 
-def timer(func):
-    def the_wrapper_around_the_original_function(*args, **kwargs):
-        start_time = time()
-        arguments = ",".join(f'{i}={kwargs[i]}' for i in kwargs)
-        result = func(*args, **kwargs)
-        stop_time = time()
-        
-        log = '''
+def log_timer(name_func, diff_time, args_):
+    arguments = ", ".join(f'{i}' for i in args_)
+    log = '''
 {stars}
 function: {function}
 arguments: {arguments}
 time: {time}
 {stars}
-            '''.format(
-                stars='*******************',
-                function=func.__name__,
-                arguments=arguments,
-                time=stop_time - start_time
-        )
+        '''.format(
+        stars='*******************',
+        function=name_func,
+        arguments=arguments,
+        time=diff_time
+    )
 
-        mode = 'a'
-        filename = 'timer.log'
+    mode = 'a'
+    filename = 'timer.log'
 
-        if not os.path.isfile(filename):
-            mode = 'w+'
+    if not os.path.isfile(filename):
+        mode = 'w+'
 
-        with open(filename, mode) as f:
-            f.write(log)
+    with open(filename, mode) as f:
+        f.write(log)
+
+
+def timer(func):
+    def the_wrapper_around_the_original_function(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        stop_time = time.time()
+
+        log_timer(func.__name__, stop_time - start_time, args)
 
         return result
 
     return the_wrapper_around_the_original_function
+
+
+def random_date(start, end, prop):
+    format = '%Y-%m-%d'
+
+    stime = time.mktime(time.strptime(start, format))
+    etime = time.mktime(time.strptime(end, format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(format, time.localtime(ptime))
 
 
 def init_postgres(func):
