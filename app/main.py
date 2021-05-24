aa519f0e1143baeda0f3bf96f1727bff7075b358
Dmitry Evseev <ew.dim@mail.ru>
2021-05-25 03:24:01 +0700

    Написан скрипт запуска программы

--- app/main.py
+++ app/main.py
@@ -1,17 +1,31 @@
-from time import sleep
-
+import os
+import tempfile
 
 from config import init_config, config
 from Errors import error_db_handler
-from Database.postgres import Postgres_db
-from Database.clickhouse import Clickhouse_db
+
+from cache import update_cache_table_clickhouse
+from generate_data_postgres import generate_data
+from script_generation_data import data_generation_scripts
+from select_data.select_postgres import select_all_query_postgres
+from select_data.select_clickhouse import select_all_query_clickhouse
 
 
 @error_db_handler
 def main():
-    # sleep(20)
     print('START PYTHON')
     init_config('/app/settings.ini')
+
+    # Create directory for temp files
+    os.makedirs(config['APP']['PATH_TEMP_FILES'], exist_ok=True)
+    tempfile.tempdir = config['APP']['PATH_TEMP_FILES']
+    
+    for indx, script in enumerate(data_generation_scripts):
+        print(f'Start {indx} script')
+        generate_data(list_count_rows=script)
+        update_cache_table_clickhouse(table_name='raw_data')
+        select_all_query_postgres(row=indx + 2)
+        select_all_query_clickhouse(row=indx + 2)
 
     return True
 
