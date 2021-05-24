d5e3618f39556d811384c6662144931ea8c48212
Dmitry Evseev <ew.dim@mail.ru>
2021-05-25 02:31:48 +0700

    Убрано логгирование у generate_data

--- app/generate_data_postgres.py
+++ app/generate_data_postgres.py
@@ -1,11 +1,9 @@
 from Errors import error_db_handler
-from select_data.utils import timer
 from Database.postgres import init_postgres
 
 
 @error_db_handler
 @init_postgres
-@timer(True)
 def generate_data(postgres_db, list_count_rows):
     postgres_db.select_data("""
         SELECT
